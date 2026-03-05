a05e295159dd3e3fd26c12e29ef99f457e725f70
shyla06seth-hub <shyla06seth@gmail.com>
2026-03-05 18:21:34 +0530

    Add files via upload

---
+++ Q-33.c
@@ -0,0 +1,78 @@
+//Convert an infix expression to postfix notation using stack.
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+// Push
+void push(char c) {
+    stack[++top] = c;
+}
+
+// Pop
+char pop() {
+    if (top == -1)
+        return -1;
+    return stack[top--];
+}
+
+// Check precedence
+int precedence(char op) {
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    if (op == '^')
+        return 3;
+    return 0;
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i, j = 0;
+
+    scanf("%s", infix);
+
+    for (i = 0; i < strlen(infix); i++) {
+        char ch = infix[i];
+
+        // If operand, add to postfix
+        if (isalnum(ch)) {
+            postfix[j++] = ch;
+        }
+        // If '(' push
+        else if (ch == '(') {
+            push(ch);
+        }
+        // If ')', pop until '('
+        else if (ch == ')') {
+            while (top != -1 && stack[top] != '(') {
+                postfix[j++] = pop();
+            }
+            pop(); // remove '('
+        }
+        // If operator
+        else {
+            while (top != -1 && precedence(stack[top]) >= precedence(ch)) {
+                postfix[j++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+    // Pop remaining operators
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+
+    printf("%s", postfix);
+
+    return 0;
+}
