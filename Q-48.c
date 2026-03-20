ab3a71cbe240bb9f1bdac8b55aee8720e304df9a
shyla06seth-hub <shyla06seth@gmail.com>
2026-03-20 08:59:38 +0530

    Add files via upload

---
+++ Q-48.c
@@ -0,0 +1,67 @@
+//Count Leaf Nodes
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int countLeaf(struct Node* root) {
+    if(root == NULL)
+        return 0;
+
+    if(root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeaf(root->left) + countLeaf(root->right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* nodes[n];
+
+    // Create nodes
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    // Link nodes (level order)
+    for(int i = 0; i < n; i++) {
+        if(nodes[i] != NULL) {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if(left < n)
+                nodes[i]->left = nodes[left];
+
+            if(right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    struct Node* root = nodes[0];
+
+    printf("%d", countLeaf(root));
+
+    return 0;
+}
