b26d49d98bc53692418f49baa479495801bbdde1
shyla06seth-hub <shyla06seth@gmail.com>
2026-03-22 13:54:06 +0530

    Add files via upload

---
+++ Q-50.c
@@ -0,0 +1,69 @@
+//BST Search
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int val) {
+    if(root == NULL)
+        return createNode(val);
+
+    if(val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+// Search in BST
+int search(struct Node* root, int key) {
+    if(root == NULL)
+        return 0;
+
+    if(root->data == key)
+        return 1;
+
+    if(key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* root = NULL;
+
+    // Build BST
+    for(int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    int key;
+    scanf("%d", &key);
+
+    if(search(root, key))
+        printf("Found");
+    else
+        printf("Not Found");
+
+    return 0;
+}
