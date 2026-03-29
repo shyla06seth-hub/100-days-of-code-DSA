b78cf7fc768a874ddbe0efef90273f6901448d0a
shyla06seth-hub <shyla06seth@gmail.com>
2026-03-29 14:06:12 +0530

    Add files via upload

---
+++ Q-57.c
@@ -0,0 +1,83 @@
+//Convert a binary tree into its mirror image by swapping left and right children at every node
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Build tree from level order
+struct Node* buildTree(int arr[], int n) {
+    struct Node* nodes[n];
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(nodes[i] != NULL) {
+            int l = 2*i + 1;
+            int r = 2*i + 2;
+
+            if(l < n) nodes[i]->left = nodes[l];
+            if(r < n) nodes[i]->right = nodes[r];
+        }
+    }
+
+    return nodes[0];
+}
+
+// Mirror function
+void mirror(struct Node* root) {
+    if(root == NULL)
+        return;
+
+    // swap left and right
+    struct Node* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+
+    mirror(root->left);
+    mirror(root->right);
+}
+
+// Inorder traversal
+void inorder(struct Node* root) {
+    if(root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    mirror(root);
+
+    inorder(root);
+
+    return 0;
+}
