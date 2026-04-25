c7a7409438ac8fd6b482859c1e193cbe37dd2c5c
shyla06seth-hub <shyla06seth@gmail.com>
2026-04-25 09:09:05 +0530

    Add files via upload

---
+++ Q-84.c
@@ -0,0 +1,37 @@
+//Implement Insertion Sort - Implement the algorithm.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[100];
+
+    // Input array
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Insertion Sort
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Shift elements greater than key
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // Insert key at correct position
+        arr[j + 1] = key;
+    }
+
+    // Output sorted array
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
