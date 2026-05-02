d30548c5acd9a7bf164c5a42a93d4360c6a94543
shyla06seth-hub <shyla06seth@gmail.com>
2026-05-02 10:09:51 +0530

    Add files via upload

---
+++ Q-91.c
@@ -0,0 +1,64 @@
+//Implement merge sort (divide, sort halves, merge)
+
+#include <stdio.h>
+
+#define MAX 100
+
+void merge(int arr[], int left, int mid, int right) {
+    int temp[MAX];
+    int i = left, j = mid + 1, k = left;
+
+    // Merge the two sorted halves
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    // Copy remaining elements (if any)
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= right)
+        temp[k++] = arr[j++];
+
+    // Copy back to original array
+    for (int x = left; x <= right; x++)
+        arr[x] = temp[x];
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+
+        // Divide
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[MAX];
+
+    // Input
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Sort
+    mergeSort(arr, 0, n - 1);
+
+    // Output
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
